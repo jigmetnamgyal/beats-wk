23760be38c41b3a942d507e6ead65498337dc521
Kzoeps <kzoepa@gmail.com>
2020-10-06 15:00:38 +0600

    ADD: about me page and styles.

---
+++ pages/aboutMe.js
@@ -0,0 +1,8 @@
+import styles from "../components/aboutMe.module.css"
+export default function AboutMe() {
+    return(
+        <div>
+            <h1 className={styles.title}>About Me</h1>
+        </div>
+    )
+}
