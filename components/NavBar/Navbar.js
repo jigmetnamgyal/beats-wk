8d13189f165090190c72d1943fdafcab9620c99c
jigmet12 <jigmetashi02@gmail.com>
2020-10-08 00:58:30 +0400

    plain navbar done

--- components/NavBar/Navbar.js
+++ components/NavBar/Navbar.js
@@ -2,18 +2,23 @@ import styles from './navbar.module.css';
 import utilStyles from "../../styles/utils.module.css";
 import Link from 'next/link';
 import Ham from './tools/HamBurger';
-
+import {FaShoppingCart} from "react-icons/fa"
+import {useState} from 'react';
 const Navbar = () => {
+    const [count, setCount] = useState(0)
     return (
         <header className={styles.navBar}>
-            <Ham />
-            <nav>
-                <div>
-                    <ul>
-                        <li><Link href="/"><a></a></Link></li>
-                    </ul>
+            <div className={styles.navBarHolder}>
+                <div className={styles.hamHolder}>
+                    <Ham/>
                 </div>
-            </nav>
+                <div className={styles.cartHolder}>
+                    <FaShoppingCart className={styles.cartIcon}/>
+                    <p>{count}</p>
+                </div>
+
+            </div>
+
         </header>
     )
 }
