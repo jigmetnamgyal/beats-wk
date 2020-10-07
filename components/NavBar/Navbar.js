fb67086799e8d87710e155d2bf107ec3425dda62
jigmet12 <jigmetashi02@gmail.com>
2020-10-07 22:41:25 +0400

    styling fixes

---
+++ components/NavBar/Navbar.js
@@ -0,0 +1,12 @@
+import styles from './navbar.module.css';
+import utilStyles from "../../styles/utils.module.css";
+
+const Navbar = () => {
+    return (
+        <div className={styles.navBar}>
+            
+        </div>
+    )
+}
+
+export default Navbar
