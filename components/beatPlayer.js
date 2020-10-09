e9133b061787a53428e422e8aff3728ab0e6ac90
Kzoeps <kzoepa@gmail.com>
2020-10-09 14:00:21 +0600

    added beats text and checkout button

---
+++ components/beatPlayer.js
@@ -0,0 +1,5 @@
+export default function BeatsPlayer() {
+    <div>
+
+    </div>
+}
--- pages/beats.js
+++ pages/beats.js
@@ -1,9 +1,40 @@
 import Button from "../components/button";
 import utilStyles from "../styles/utils.module.css";
+import beatsStyles from "../components/beats.module.css";
+import {Col, Container, Row} from "react-bootstrap";
+import NavigationBar from "../components/NavBar/NavigationBar";
+import styles from "../components/aboutMe.module.css";
+
 export default function Beats() {
-    return(
-        <div>
-            <Button buttonLabel="Checkout"/>
-        </div>
+    return (
+        <Container className={`${utilStyles.backgroundColor}`}>
+            <NavigationBar/>
+            <Row className={`justify-content-center ${utilStyles.headers}`}>
+                <Col xs={11}>
+                    <h3 className={utilStyles.title}>Beats</h3>
+                    <hr className={utilStyles.hr}/>
+                </Col>
+            </Row>
+            <Row className="justify-content-center">
+                <Col xs={8} className={`${utilStyles.text} ${styles.text}`}>
+                    <p>
+                        My beats are solely produced by me and i make them for fun but would be down for a collaboration
+                        as well.
+                    </p>
+                    <p>
+                        The beats are licensed under apache 2.0 and please dont fork.
+                        All beats are pretty good and if you really want to use but no money then email me.
+                    </p>
+                </Col>
+                <Col xs={3}>
+                    POLPPPP
+                </Col>
+            </Row>
+            <Row className="no-gutters">
+                <Col xs={{span:4, offset: 7}} className={beatsStyles.button}>
+                    <Button buttonLabel="Checkout"/>
+                </Col>
+            </Row>
+        </Container>
     )
 }
