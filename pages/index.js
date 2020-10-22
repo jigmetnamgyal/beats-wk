a05dd21a87a251a6d7b76bccf567ee9821975f27
jigmet12 <jigmetashi02@gmail.com>
2020-10-22 13:42:21 +0400

    ADDED: comment

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,32 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import NavBar from '../components/NavBar/Navbar';
 import Footer from '../components/footer';
-import SideBar from '../components/NavBar/tools/SideBar';
-import { useState } from 'react';
 import Navagation from '../components/NavBar/NavigationBar';
+import Playlist from '../components/playlist/Playlist';
 export default function Home() {
-
-    // const [toggleFlag, setToggleFlag] = useState(false);
-
-    // const hamHandler = () => {
-    //     if(!toggleFlag){
-    //         setToggleFlag(true)
-    //     }
-        
-    // }
-
-    // const closeHandler = () => {
-    //     setToggleFlag(false);
-    // }
-
-    
 
     return (
         <div className={styles.container}>
             <Head>
-                <title>Create Next App</title>
+                <title>Wk Beats</title>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
-
-            <main className={styles.main}>
-                {/* <NavBar hamHandler={hamHandler}/>
-                {toggleFlag && <SideBar close={closeHandler} show={toggleFlag}/>} */}
-                <Navagation />
+            <Navagation/>
+            <main className={styles.mainPage}>
+                <div className={styles.mainContentContainer}>
+                    <div className={styles.homeHeadingContainer}>
+                        <p className={styles.headingOne}>#1 market place to buy beats</p>
+                        <p className={styles.headingTwo}>We have more than 1.2 m happy customers</p>
+                    </div>
+                </div>
+               
             </main>
-
+            <div className={styles.homepagePlaylistContainer}>
+                {/* For now the image link is directly passed as props , when db is ready will do in the component itself */}
+                    <Playlist title="Recommended Playlist" imageOne="https://images.genius.com/8ababae027808d8f2de49bfcfc51488d.592x592x1.png" imageTwo="https://upload.wikimedia.org/wikipedia/en/5/52/HeadInTheClouds-Album.jpg"/>
+                    <Playlist title="Popular Playlist" imageTwo="https://images.genius.com/8ababae027808d8f2de49bfcfc51488d.592x592x1.png" imageOne="https://upload.wikimedia.org/wikipedia/en/5/52/HeadInTheClouds-Album.jpg" />
+                    <Playlist title="Lorem Ipsum Playlist" imageOne="https://images.genius.com/8ababae027808d8f2de49bfcfc51488d.592x592x1.png" imageTwo="https://upload.wikimedia.org/wikipedia/en/5/52/HeadInTheClouds-Album.jpg" />
+            </div>
             <Footer/>
         </div>
     )
