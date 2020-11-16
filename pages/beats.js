bbb35d6473d6b925102a67f13db03c24e73eff08
Kzoeps <kzoepa@gmail.com>
2020-11-16 21:24:38 +0600

    FEAT: Beats Page for computer

--- pages/beats.js
+++ pages/beats.js
@@ -8,6 +8,7 @@ import BeatsPlayer from "../components/beatPlayer";
 import Footer from "../components/footer";
 import LicenseForm from "../components/licenseForm";
 import Link from "next/link";
+
 export default function Beats() {
     return (
         <Container className={`${utilStyles.backgroundColor} ${beatsStyles.metaContainer}`} fluid={true}>
@@ -36,9 +37,11 @@ export default function Beats() {
             <div className={`${beatsStyles.beatsDescriptionComp}`}>
                 <Col lg={{span: 11, offset: 1}} className={`${beatsStyles.beatsDescriptionContainer}`}>
                     <Row>
-                        <Col lg={{span: 5}} className={`${utilStyles.text} ${styles.text} ${beatsStyles.text} ${beatsStyles.textContainer}`}>
+                        <Col lg={{span: 5}}
+                             className={`${utilStyles.text} ${styles.text} ${beatsStyles.text} ${beatsStyles.textContainer}`}>
                             <p>
-                                My beats are solely produced by me and i make them for fun but would be down for a collaboration
+                                My beats are solely produced by me and i make them for fun but would be down for a
+                                collaboration
                                 as well.
                             </p>
                             <p>
@@ -50,24 +53,85 @@ export default function Beats() {
                 </Col>
             </div>
             <Row className="no-gutters">
-                <Col xs={{span:4, offset: 7}} className={beatsStyles.button}>
+                <Col xs={{span: 4, offset: 7}} className={beatsStyles.button}>
                     <Button buttonLabel="Checkout"/>
                 </Col>
             </Row>
-            <Row className='no-gutters justify-content-center'>
-                <Col xs={12} className={beatsStyles.beatPlayer}>
-                    <BeatsPlayer beatDuration="2:59" imgURL="https://images.genius.com/8ababae027808d8f2de49bfcfc51488d.592x592x1.png"/>
-                </Col>
-                <Col xs={12} className={beatsStyles.beatPlayer}>
-                    <BeatsPlayer beatDuration="3:00" imgURL={"https://upload.wikimedia.org/wikipedia/en/5/52/HeadInTheClouds-Album.jpg"}/>
-                </Col>
-                <Col xs={12} className={beatsStyles.beatPlayer}>
-                    <BeatsPlayer beatDuration="1:54" imgURL={"https://upload.wikimedia.org/wikipedia/en/1/10/Childish_Gambino_-_Awaken%2C_My_Love%21.png"}/>
-                </Col>
-                <Col xs={12} className={beatsStyles.beatPlayer}>
-                    <BeatsPlayer beatDuration="1:23" imgURL={"https://upload.wikimedia.org/wikipedia/en/5/52/HeadInTheClouds-Album.jpg"}/>
-                </Col>
-            </Row>
+            <div className={`${beatsStyles.cartAndBeats}`}>
+                <div className={`${beatsStyles.beatsPlayer} no-gutters`}>
+                    <div className={beatsStyles.beatPlayer}>
+                        <BeatsPlayer beatDuration="2:59"
+                                     imgURL="https://images.genius.com/8ababae027808d8f2de49bfcfc51488d.592x592x1.png"/>
+                    </div>
+                    <div className={beatsStyles.beatPlayer}>
+                        <BeatsPlayer beatDuration="3:00"
+                                     imgURL={"https://upload.wikimedia.org/wikipedia/en/5/52/HeadInTheClouds-Album.jpg"}/>
+                    </div>
+                    <div className={beatsStyles.beatPlayer}>
+                        <BeatsPlayer beatDuration="1:54"
+                                     imgURL={"https://upload.wikimedia.org/wikipedia/en/1/10/Childish_Gambino_-_Awaken%2C_My_Love%21.png"}/>
+                    </div>
+                    <div className={beatsStyles.beatPlayer}>
+                        <BeatsPlayer beatDuration="1:23"
+                                     imgURL={"https://upload.wikimedia.org/wikipedia/en/5/52/HeadInTheClouds-Album.jpg"}/>
+                    </div>
+                </div>
+                <div className={`${beatsStyles.cartSummary}`}>
+                    <div className={`${beatsStyles.cartSummaryTitle}`}>
+                        <h3 className={`${utilStyles.headers} ${utilStyles.title} ${beatsStyles.title}`}>Cart Summary</h3>
+                        <hr className={`${utilStyles.hr}`}/>
+                    </div>
+                    <div className={`${beatsStyles.cartSummaryItemPrice}`}>
+                        <div className={`${utilStyles.text}`}>
+                            <p>Item</p>
+                        </div>
+                        <div className={`${utilStyles.text}`}>
+                            <p>Price</p>
+                        </div>
+                    </div>
+                    <hr className={`${beatsStyles.cartSummaryhr}`}/>
+                    <div className={`${beatsStyles.items}`}>
+                        <div className={`${beatsStyles.eachItem}`}>
+                            <div className={`${utilStyles.text}`}>
+                                Song Name
+                            </div>
+                            <div className={`${utilStyles.text}`}>
+                                30$
+                            </div>
+                        </div>
+                        <div className={`${beatsStyles.eachItem}`}>
+                            <div className={`${utilStyles.text}`}>
+                                Toosie Slide
+                            </div>
+                            <div className={`${utilStyles.text}`}>
+                                30$
+                            </div>
+                        </div>
+                        <div className={`${beatsStyles.eachItem}`}>
+                            <div className={`${utilStyles.text}`}>
+                                Black Leather Gloves
+                            </div>
+                            <div className={`${utilStyles.text}`}>
+                                30$
+                            </div>
+                        </div>
+                    </div>
+                    <hr className={`${beatsStyles.totalHR}`}/>
+                    <div className={`${beatsStyles.items}`}>
+                        <div className={`${beatsStyles.eachItem}`}>
+                            <div className={`${utilStyles.text}`}>
+                                Total
+                            </div>
+                            <div className={`${utilStyles.text}`}>
+                                90$
+                            </div>
+                        </div>
+                    </div>
+                    <div className={`${beatsStyles.checkoutButton}`}>
+                        <Button buttonLabel="Checkout"/>
+                    </div>
+                </div>
+            </div>
             <Footer/>
         </Container>
     )
