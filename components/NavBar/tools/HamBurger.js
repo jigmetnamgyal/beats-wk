1a3ab669a502cbffcb78d993580a20b20865f1fb
jigmet12 <jigmetashi02@gmail.com>
2020-12-13 12:55:47 +0400

    FIXED: fixed the menu icon

--- components/NavBar/tools/HamBurger.js
+++ components/NavBar/tools/HamBurger.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import styles from './ham.module.css';
+import {HiOutlineMenuAlt1} from 'react-icons/hi';
 
 const HamBurger = ({click}) => {
     return (
         <button className={styles.hamburger} onClick={click}>
-            <div className={styles.hamLine}></div>
-            <div className={styles.hamLine}></div>
-            <div className={styles.hamLine}></div>
+            <HiOutlineMenuAlt1 className={styles.hamLine}/>
         </button>
     )
 }
