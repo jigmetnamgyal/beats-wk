870b7c855ca94115db2bc0bef4c99fcfdec2d58b
JigmeT12 <jigmetashi02@gmail.com>
2021-04-04 21:12:56 +0600

    UPDATED: footer info

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -12,16 +12,29 @@ const Footer = () => {
                         <h4>WK Beats</h4>
                     </div>
 
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."</p>
+                    <p>Yo! I am Wangchuk Kinga Beats, a Bhutanese beat-maker based in Bhutan. I am a
+                        full time beat-maker and I am more into hip hop and R&B music. Every beat I
+                        create is purely based on my creativity and I assure the originality of every
+                        beat I own. I am determined to work hard and fulfill my dreams. I hope ya’all
+                        like my beats and looking forward to work with ya’all in near future.</p>
                     <div className={styles.footerOneMediaContainer}>
-                        <a href="https://www.instagram.com/wangchuk_k/" rel="noopener noreferrer" target="_blank" className={styles.footerInsta}><AiOutlineInstagram/>
+                        <a
+                            href="https://www.instagram.com/wangchuk_k/"
+                            rel="noopener noreferrer"
+                            target="_blank"
+                            className={styles.footerInsta}><AiOutlineInstagram/>
                         </a>
-                        <a href="https://www.facebook.com/Wangchuk-Kinga-Beats-2199505916803723" rel="noopener noreferrer" target="_blank" className={styles.footerFacebook}><AiFillFacebook/>
+                        <a
+                            href="https://www.facebook.com/Wangchuk-Kinga-Beats-2199505916803723"
+                            rel="noopener noreferrer"
+                            target="_blank"
+                            className={styles.footerFacebook}><AiFillFacebook/>
                         </a>
                         {/* <a href="/" rel="noopener noreferrer" target="_blank" className={styles.footerTwitter}><AiOutlineTwitter/>
                         </a>
                         <a href="/" rel="noopener noreferrer" target="_blank" className={styles.footerLinkedin}><AiFillLinkedin/>
-                        </a> */}
+                        </a> */
+                        }
                     </div>
                 </div>
 
@@ -29,13 +42,13 @@ const Footer = () => {
                     <h4>Quick Links</h4>
                     <Link href="/">Home</Link>
                     <Link href="/beats">Beats</Link>
-                    
+
                 </div>
                 <div className={styles.footerFour}>
                     <h4>About Us</h4>
                     <Link href="/aboutMe">About Us</Link>
                     <Link href="/contactUs">Contact Us</Link>
-                   
+
                 </div>
                 <div className={styles.footerThree}>
                     <h4>Legal</h4>
@@ -48,8 +61,9 @@ const Footer = () => {
             <div className={styles.footerChildTwoContainer}>
                 <div className={styles.footerTwoContainer}>
                     <p>
-                        &copy;Wk Beats Bhutan 2020-
-                        <span> get your beat</span>
+                        &copy;Wk Beats Bhutan 2021-
+                        <span>
+                            get your beat</span>
                     </p>
                 </div>
             </div>
